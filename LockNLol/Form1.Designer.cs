374d9a76809aa45baa6e19e11e06ca615cead132
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2022-06-15 12:53:42 +0200

    init

---
+++ LockNLol/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace locknlol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.chkEnabled = new System.Windows.Forms.CheckBox();
+            this.chkLog = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(628, 307);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnReload);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.chkEnabled);
+            this.panel1.Controls.Add(this.chkLog);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 24);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(118, 0);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(75, 23);
+            this.btnReload.TabIndex = 3;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(195, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // chkEnabled
+            // 
+            this.chkEnabled.AutoSize = true;
+            this.chkEnabled.Checked = true;
+            this.chkEnabled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEnabled.Location = new System.Drawing.Point(3, 3);
+            this.chkEnabled.Name = "chkEnabled";
+            this.chkEnabled.Size = new System.Drawing.Size(59, 17);
+            this.chkEnabled.TabIndex = 1;
+            this.chkEnabled.Text = "Enable";
+            this.chkEnabled.UseVisualStyleBackColor = true;
+            // 
+            // chkLog
+            // 
+            this.chkLog.AutoSize = true;
+            this.chkLog.Location = new System.Drawing.Point(68, 3);
+            this.chkLog.Name = "chkLog";
+            this.chkLog.Size = new System.Drawing.Size(44, 17);
+            this.chkLog.TabIndex = 0;
+            this.chkLog.Text = "Log";
+            this.chkLog.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Lock \'n\' lol";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 338);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lock \'n\' lol";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkLog;
+        private System.Windows.Forms.CheckBox chkEnabled;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReload;
+    }
+}
+
---
+++ LockNLol/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace locknlol
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+
+        [StructLayoutAttribute(LayoutKind.Sequential)]
+        private struct DEV_BROADCAST_HDR
+        {
+            public uint dbch_size;
+            public uint dbch_devicetype;
+            public uint dbch_reserved;
+        }
+
+        private const uint DBT_DEVTYP_VOLUME = 0x00000002;
+        private const uint DBT_DEVTYP_DEVICEINTERFACE = 0x00000005;
+        private const ushort DBTF_MEDIA = 0x0001; // Media in drive changed.
+        private const ushort DBTF_NET = 0x0002; // Network drive is changed.
+
+        [StructLayoutAttribute(LayoutKind.Sequential)]
+        private struct DEV_BROADCAST_VOLUME
+        {
+            public uint dbcv_size;
+            public uint dbcv_devicetype;
+            public uint dbcv_reserved;
+            public uint dbcv_unitmask;
+            public ushort dbcv_flags;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct DEV_BROADCAST_DEVICEINTERFACE
+        {
+            public uint dbcc_size;
+            public uint dbcc_devicetype;
+            public uint dbcc_reserved;
+            public Guid dbcc_classguid;
+
+            // To get value from lParam of WM_DEVICECHANGE, this length must be longer than 1.
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
+            public string dbcc_name;
+        }
+
+        private bool AllowDisplay = false;
+
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(AllowDisplay ? value : AllowDisplay);
+        }
+
+        string[] lockDevices = new string[] {  };
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            DeviceNotification.RegisterUsbDeviceNotification(this.Handle, false);
+            RefreshConfigFile();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            
+            if (m.Msg == DeviceNotification.WmDeviceChange)
+            {
+                switch ((int)m.WParam)
+                {
+                    case DeviceNotification.DbtDeviceRemoveComplete:
+                        Write("Removed!");
+                        HandleMessage(m.LParam, true);
+                        Write("---");
+                        break;
+                    case DeviceNotification.DbtDeviceArrival:
+                        Write("Added!");
+                        HandleMessage(m.LParam, false);
+                        Write("---");
+                        break;
+                }
+            }
+        }
+
+        private void HandleMessage(IntPtr lParam, bool removed)
+        {
+            var hdr = (DEV_BROADCAST_HDR)Marshal.PtrToStructure(lParam, typeof(DEV_BROADCAST_HDR));
+            var deviceInterface = (DEV_BROADCAST_DEVICEINTERFACE)Marshal.PtrToStructure(lParam, typeof(DEV_BROADCAST_DEVICEINTERFACE));
+
+            uint devType = hdr.dbch_devicetype;
+            string devName = deviceInterface.dbcc_name;
+
+            // 6e8cf83&0&Yubico_YubiKey
+            Write(devType.ToString());
+            Write($"dbcc_name = {deviceInterface.dbcc_name}");
+            Write($"dbcc_devicetype = {deviceInterface.dbcc_devicetype}");
+            Write($"dbcc_classguid = {deviceInterface.dbcc_classguid}");
+
+            // ACS_ACR122U_PICC_Interface_0_SCFILTER_CID_8073c021c057597562694b65ff
+            // \\?\USB#VID_2109&PID_8887#40AN
+            // 446c009&0&Yubico_YubiKey
+
+            if (removed && chkEnabled.Checked) {
+                foreach(string lockDev in lockDevices)
+                {
+                    if (Regex.IsMatch(devName, lockDev))
+                        LockWorkStation();
+                }
+            } 
+
+        }
+
+        void RefreshConfigFile()
+        {
+            string path = Path.Combine(AssemblyDirectory, "locknlol.conf");
+            if (File.Exists(path))
+                this.lockDevices = File.ReadAllLines(path);
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        void Write(string Message)
+        {
+            if(chkLog.Checked)
+                textBox1.AppendText(Message + Environment.NewLine);
+        }
+
+
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.AllowDisplay = true;
+            this.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Application.Exit();
+            Environment.Exit(0);
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            RefreshConfigFile();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
